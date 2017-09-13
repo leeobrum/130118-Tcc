68aa60d01ab381ba7a1c7e7579de115e4b5fc2a5
Leonardo Brum <leeobrum@gmail.com>
2017-09-12 21:47:11 -0300

    Pagina de detalhe do Produto

--- public/js/init.js
+++ public/js/init.js
@@ -2,3 +2,13 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$(".slider").slider({full_widt: true});
 });
+
+function sliderPrev(){
+	$('.slider').slider('pause');
+	$('.slider').slider('prev');
+}
+
+function sliderNext(){
+	$('.slider').slider('pause');
+	$('.slider').slider('next');
+}
--- resources/views/site/produto.blade.php
+++ resources/views/site/produto.blade.php
@@ -13,17 +13,35 @@
 					<ul class="slides">
 						<li>
 							<img src="{{asset('img/janela_1.jpg')}}">
+							<div class="caption center-align">
+								<h3>Título da Imagem</h3>
+								<h5>Descrição do Slide</h5>
+							</div>
+						</li>
+						<li>
+							<img src="{{asset('img/janela_1.jpg')}}">
+							<div class="caption left-align">
+								<h3>Título da Imagem</h3>
+								<h5>Descrição do Slide</h5>
+							</div>
 						</li>
 					</ul>
 				</div>
 			</div>
+			<div class="row" align="center">
+				<button onclick="sliderPrev()" class="btn green">Anterior</button>
+				<button onclick="sliderNext()" class="btn green">Proxima</button>
+			</div>
 		</div>
 		<div class="col s12 m4">
-			<h4>A Empresa</h4>
+			<h4>Titulo do Produto</h4>
 			<blockquote>
-				Descrição breve sobre a empresa.
+				Descrição breve sobre o produto.
 			</blockquote>
-			<p>Testo Sobre a Empresa</p>
+			<p><b>Codigo: </b> 245 </p>
+			<p><b>Status: </b> Ativo </p>
+			<p><b>Valor: </b> R$ 23,00 </p>
+			<a class="btn deep-orange darken-1" href="{{route('site.contato')}}">Comprar</a>
 		</div>
 	</div>
 </div>
